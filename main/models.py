58b715c954b273eb965e868b8115ebbb2279cddd
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-09-07 20:44:50 +0700

    Membuat model Product pada app main

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=255)
+    is_featured = models.BooleanField()
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
