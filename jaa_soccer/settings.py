533e3c2223e15ae5bf33ca487ca39ed31b1448a8
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-09-14 20:01:51 +0700

    Menambahkan CSRF trusted origins

--- jaa_soccer/settings.py
+++ jaa_soccer/settings.py
@@ -31,6 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mirza-radithya-jaasoccer.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://mirza-radithya-jaasoccer.pbp.cs.ui.ac.id"
+]
 
 
 # Application definition
