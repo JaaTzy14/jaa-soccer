f4d90c3561d04f8d201d2efaeb5f246196ce9f8b
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-09-14 17:59:52 +0700

    menambahkan 4 fungsi views baru untuk melihat objek yang sudah ditambahkan dalam format XML, JSON, XML by ID, dan JSON by ID

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from main.models import Product
 
 # Create your views here.
 
@@ -10,3 +13,23 @@ def show_main(request):
         'class': "PBP B"
     }
     return render(request, "main.html", context)
+
+def show_xml(request):
+    productList = Product.objects.all()
+    xmlData = serializers.serialize("xml", productList)
+    return HttpResponse(xmlData, content_type="application/xml")
+
+def show_json(request):
+    productList = Product.objects.all()
+    jsonData = serializers.serialize("json", productList)
+    return HttpResponse(jsonData, content_type="application/json")
+
+def show_xml(request, id):
+    product = Product.objects.filter(pk=id)
+    xmlData = serializers.serialize("xml", product)
+    return HttpResponse(xmlData, content_type="application/xml")
+
+def show_json(request, id):
+    productList = Product.objects.filter(pk=id)
+    jsonData = serializers.serialize("json", productList)
+    return HttpResponse(jsonData, content_type="application/json")
