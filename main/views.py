742a5cf5a2a33d4b2a3c34f18026a97e6443e471
JaaTzy14 <mirzaradithya1410@gmail.com>
2025-09-28 19:42:21 +0700

    Tugas 5 belum README

--- main/views.py
+++ main/views.py
@@ -12,13 +12,24 @@ from django.urls import reverse
 
 # Create your views here.
 
-@login_required(login_url='/login')
 def show_main(request):
     filter_type = request.GET.get("filter", "all")
-    if filter_type == "all":
-        productList = Product.objects.all()
-    else:
+    if filter_type == "my" and request.user.is_authenticated:
         productList = Product.objects.filter(user=request.user)
+    elif filter_type == "jersey":
+        productList = Product.objects.filter(category='jersey')
+    elif filter_type == "ball":
+        productList = Product.objects.filter(category='ball')
+    elif filter_type == "boots":
+        productList = Product.objects.filter(category='boots')
+    elif filter_type == "accessories":
+        productList = Product.objects.filter(category='accessories')
+    elif filter_type == "merchandise":
+        productList = Product.objects.filter(category='merchandise')
+    elif filter_type == "training":
+        productList = Product.objects.filter(category='training')
+    else:
+        productList = Product.objects.all()
     context =  {
         'appName': "Jaa Soccer",
         'name': request.user,
@@ -30,6 +41,7 @@ def show_main(request):
     }
     return render(request, "main.html", context)
 
+@login_required(login_url='/login')
 def create_product(request):
     form = ProductForm(request.POST or None)
 
@@ -39,17 +51,20 @@ def create_product(request):
         product_entry.save()
         return redirect('main:show_main')
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'name': request.user,
+        'npm': 2406405563,
+        'class': "PBP B",
+        }
     return render(request, "create_product.html", context)
 
-@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     context = {
         'product': product
     }
     return render(request, "product_detail.html", context)
-
 
 def show_xml(request):
     productList = Product.objects.all()
@@ -91,7 +106,7 @@ def register(request):
 
 def logout_user(request):
     logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
+    response = HttpResponseRedirect(reverse('main:show_main'))
     response.delete_cookie('last_login')
     return response
 
@@ -102,7 +117,11 @@ def login_user(request):
       if form.is_valid():
         user = form.get_user()
         login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
+        next_url = request.POST.get('next') or request.GET.get('next')
+        if next_url:
+            response = HttpResponseRedirect(next_url)
+        else:
+            response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
 
@@ -110,3 +129,23 @@ def login_user(request):
       form = AuthenticationForm(request)
    context = {'form': form}
    return render(request, 'login.html', context)
+
+@login_required(login_url='/login')
+def edit_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_product.html", context)
+
+@login_required(login_url='/login')
+def delete_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
